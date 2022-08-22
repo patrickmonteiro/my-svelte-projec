8541c883d937e6a98f8cbb1ac7a78aff607b5840
Patrick <eng.patrickmonteiro@gmail.com>
2022-08-22 20:09:09 -0300

    create cards user

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+import BarraSuperior from './components/BarraSuperior.svelte';
 import Titulo from './components/Titulo.svelte';
+import type IUsuario from './interfaces/IUsuario';
 
 let valorInput = 'Patrick';
+
+let usuario: IUsuario = {
+  avatar_url: 'https://github.com/patrickmonteiro.png',
+  login: 'patrickmonteiro',
+  nome: 'Patrick Monteiro',
+  perfil_url: 'https://github.com/patrickmonteiro',
+  repositorios_publicos: 50,
+  seguidores: 100
+}
 
 function handleSubmit() {
   
@@ -21,6 +32,33 @@ function handleSubmit() {
       </form>
     </div>
   </header>
+  <div class="card-usuario">
+    <BarraSuperior />
+    <div class="usuario">
+      <div class="foto-container">
+        <a href={usuario.perfil_url} target="_blank" rel="noopener">
+          <div
+            class="foto-usuario"
+            style:background-image="url({usuario.avatar_url})"
+          ></div>
+        </a>
+      </div>
+      <div class="detalhes-usuario">
+        <div class="info">
+          Nome: <span>{usuario.nome}</span>
+        </div>
+        <div class="info">
+          Usuario: <span>{usuario.login}</span>
+        </div>
+        <div class="info">
+          Seguidores: <span>{usuario.seguidores}</span>
+        </div>
+        <div class="info">
+          Repositórios: <span>{usuario.repositorios_publicos}</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <style>
@@ -80,5 +118,47 @@ header {
 .botao:hover {
   background: #4590ff;
 }
+
+.card-usuario {
+    margin-top: 65px;
+  }
+
+  .usuario {
+    padding: 28px 0;
+    background: rgba(255, 255, 255, 0.5);
+    box-shadow: -12px 37px 45px rgba(133, 127, 201, 0.18);
+    border-radius: 0px 0px 13px 13px;
+
+    display: flex;
+    justify-content: center;
+  }
+
+  .foto-container {
+    margin-right: 81px;
+  }
+
+  .foto-usuario {
+    width: 12.75rem;
+    height: 12.75rem;
+    border: 4.56px solid #2e80fa;
+    border-radius: 50%;
+    background-size: cover;
+  }
+
+  .detalhes-usuario {
+    margin-right: 55px;
+  }
+
+  .detalhes-usuario > .info {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 31px;
+    color: #395278;
+  }
+
+  .detalhes-usuario > .info > span {
+    color: #6781a8;
+    font-weight: normal;
+  }
 
 </style>
