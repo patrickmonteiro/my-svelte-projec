9cc3119e17b66296ac689caad038324035522ada
Patrick <eng.patrickmonteiro@gmail.com>
2022-08-21 21:59:40 -0300

    create form and submit function

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,84 @@
 <script lang="ts">
-	export let name: string;
-	export const description: string = 'Essa é uma descrição';
-	export const src: string = 'https://cdn.pixabay.com/photo/2014/10/30/21/41/tutankhamun-509752_1280.jpg';
-	export const alt: string = 'Egito'
+import Titulo from './components/Titulo.svelte';
+
+let valorInput = 'Patrick';
+
+function handleSubmit() {
+  
+  console.log(valorInput);
+}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>{description}</p>
-	<img {src} {alt} height="300px">
-</main>
+<div class="app">
+  <header>
+    <Titulo />
+    <div class="busca-usuario">
+      <form on:submit|preventDefault={handleSubmit}>
+        <input type="text" class="input" bind:value={valorInput}>
+        <div class="botao-container">
+          <button type="submit" class="botao">Buscar</button>
+        </div>
+      </form>
+    </div>
+  </header>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+.app {
+  max-height: 100vh;
+}
+header {    
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.busca-usuario {
+    position: relative;
+    width: 70%;
+  }
+.input {
+  padding: 15px 25px;
+  width: calc(100% - 8.75rem);
+  font-size: 1rem;
+  border-radius: 8px;
+  border: 1px solid #2e80fa;
+  box-shadow: 0px 17px 52px rgba(222, 231, 247, 0.4);
+  outline: 0;
+}
+.input::placeholder {
+  font-family: "Roboto";
+  font-style: italic;
+  font-weight: 300;
+  font-size: 19.5px;
+  line-height: 26px;
+  color: #6e8cba;
+}
+.botao-container {
+  position: absolute;
+  width: 9.625rem;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+}
+.botao {
+  padding: 15px 24px;
+  border-radius: 8px;
+  border: none;
+  background: #2e80fa;
+  line-height: 26px;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  transition: background-color 0.2s;
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  display: flex;
+  align-items: center;
+  gap: 13px;
+}
+.botao:hover {
+  background: #4590ff;
+}
+
 </style>
